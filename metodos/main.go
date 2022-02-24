cdc891d129ac6816dce88197268cce5e4b0f2d2d
jairodias <jairodias@MacBook-Jairo.local>
2022-02-24 06:24:13 -0300

    aprendendo sobre metodos no struct

---
+++ metodos/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type usuario struct {
+	nome  string
+	idade uint8
+}
+
+func (u usuario) salvar() {
+	fmt.Printf("Salvando os dados do usuário %s no banco de dados\n", u.nome)
+}
+
+func (u usuario) maiorDeIdade() bool {
+	return u.idade >= 18
+}
+
+func (u *usuario) fazerAniversario() {
+	u.idade++
+}
+
+func main() {
+	usuario1 := usuario{"Jairo Dias", 22}
+	usuario1.salvar()
+
+	usuario2 := usuario{"Fulano", 23}
+	usuario2.salvar()
+	maiorDeIdade := usuario2.maiorDeIdade()
+	println(maiorDeIdade)
+
+	usuario2.fazerAniversario()
+	fmt.Println(usuario2)
+}
