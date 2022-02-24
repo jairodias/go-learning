8666d80a82fc9f671c566cb6ad5e438382c86c6c
jairodias <jairodias@MacBook-Jairo.local>
2022-02-24 06:39:03 -0300

    aprendendo sobre interfaces genéricas

---
+++ interfaces/tipo-generico/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func generica(interf interface{}) {
+	fmt.Println(interf)
+}
+
+func main() {
+	generica("String")
+	generica(1)
+	generica(true)
+
+	mapa := map[interface{}]interface{}{
+		1:            "String",
+		float32(100): true,
+	}
+
+	fmt.Println(mapa)
+}
