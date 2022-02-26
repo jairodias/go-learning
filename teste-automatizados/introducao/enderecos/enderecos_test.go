b92c66bbf878b2db6994e41417580e0be2725de6
jairodias <jairodias@MacBook-Jairo.local>
2022-02-26 07:48:22 -0300

    aprendendo sobre testes unitarios

---
+++ teste-automatizados/introducao/enderecos/enderecos_test.go
@@ -0,0 +1,29 @@
+package enderecos
+
+import "testing"
+
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
+func TestTipoDeEndereco(t *testing.T) {
+	cenariosDeTeste := []cenarioDeTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Praça das Rosas", "Tipo inválido"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"ESTRADA QUALQUER", "Estrada"},
+		{"", "Tipo inválido"},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s",
+				cenario.retornoEsperado,
+				tipoDeEnderecoRecebido)
+		}
+	}
+
+}
